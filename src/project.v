feebf1b09ffd6f09cebb6cf34b4513e1feefd7f1
Elisa Tan <elisatan@eduroam-campus-10-36-218-191.campus-dynamic.uwaterloo.ca>
2026-09-24 18:14:10 -0400

    code refactor

--- src/project.v
+++ src/project.v
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2026 Elisa
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -17,11 +17,10 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
-
-endmodule
+  //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  //assign uio_out = 0;
+  //assign uio_oe  = 0;
+  
+  // Give the control inputs readable names
+  wire en   = ui_in[0];  // 1 = count up
+  wire load =
